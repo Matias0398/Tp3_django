68f19a891a477a2e1784bf1d38ca953c052ee5e3
Matias0398 <matias.llanos1998@gmail.com>
2025-06-11 19:43:17 -0300

    agrego funcion try-except

--- tienda/views.py
+++ tienda/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from .models import Producto
 from .forms import ProductoForm, ClienteForm, BuscarProductoForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'base.html')
@@ -10,21 +11,35 @@ def home(request):
 def agregar_producto(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        try:
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Producto guardado correctamente.")
+                return redirect('home')
+            else:
+                messages.warning(request, "Formulario inválido. Revisá los datos ingresados.")
+        except Exception as e:
+            messages.error(request, f"Error inesperado al guardar el producto: {str(e)}")
     else:
         form = ProductoForm()
+    
     return render(request, 'agregar_producto.html', {'form': form})
 
 def agregar_cliente(request):
     if request.method == 'POST':
         form = ClienteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        try:
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Cliente agregado exitosamente.")
+                return redirect('home')
+            else:
+                messages.error(request, "Formulario inválido. Verificá los datos ingresados.")
+        except Exception as e:
+            messages.error(request, f"Ocurrió un error al guardar el cliente: {str(e)}")
     else:
         form = ClienteForm()
+
     return render(request, 'agregar_cliente.html', {'form': form})
 
 def buscar_producto(request):
